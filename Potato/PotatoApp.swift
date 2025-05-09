47f74c0af74dc93da1f267a9223d96a4272e507e
Raniyah <138391@swcsd.us>
2025-05-09 08:15:50 -0400

    Initial Commit

---
+++ Potato/PotatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotatoApp.swift
+//  Potato
+//
+//  Created by Raniyah Moreland on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PotatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
