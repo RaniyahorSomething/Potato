872bd7fa276e79c8b5baf8c86fe1b7acbcf5035b
Raniyah <138391@swcsd.us>
2025-05-09 08:25:25 -0400

    test thing

--- Potato/ContentView.swift
+++ Potato/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Raniyah Moreland, French Fry")
+            Text(":D      D:")
         }
         .padding()
     }
